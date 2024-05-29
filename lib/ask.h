2548eb6ee02a8131b317cb3cea2d0360c0c44e02
hedi-ben-maouiia <teedydeev@gmail.com>
2024-05-29 19:09:55 +0100

    add signUp and save to data base

--- lib/ask.h
+++ lib/ask.h
@@ -7,8 +7,13 @@
 
 #define LINE_BUFFER_SIZE 1024
 
+static size_t USER_ID = 0;
+static size_t QUISITON_ID = 0;
+
+
 typedef struct {
     size_t user_id;
+    char*  name; 
     char*  user_name;
     char*  user_email;
     char*  password;
@@ -24,15 +29,19 @@ typedef struct {
 }quistion_info;
 
 
-void read_from_file(const char* path,vector* v);
-void write_to_file(const char* path, vector* v, bool apend);
-void split_string(char *str,vector * v,const char delim);     
-int  print_menu(vector* v);
+void  read_from_file(const char* path,vector* v);
+void  write_to_file(const char* path, vector* v, bool apend);
+void  split_string(char *str,vector * v,const char delim);     
+int   to_int(char* str);
+char* to_str(int n);
+int   print_menu(vector* v);
+char* user_to_string(user_info* new_user);
+char* quition_to_string(quistion_info* new_quitions);
 
-//TODO:
-void user_init(user_info* u, char* str);
-void quistion_init(quistion_info* q, char* str);
+user_info* user(user_info* new_user);       // initialize new_user with empty value
+void  user_init(user_info* u, char* str);   // initialize an already user existed
+void  quistion_init(quistion_info* q, char* str);
 
-
+void run_system(void);
 
 #endif
--- main.c
+++ main.c
@@ -3,19 +3,7 @@
 
 
 
-int main(void){
-    vector lines, split_vec; 
-    vector_init(&split_vec);
-    vector_init(&lines);
-
-    read_from_file("users.txt",&lines);
-    split_string(lines.items[0],&split_vec,',');
-    write_to_file("quitions.txt",&split_vec,true);
-
-    print_vector(&lines);
-    print_vector(&split_vec);
-
-    vector_free(&lines);
-    vector_free(&split_vec);
-
+int main(void){ 
+    run_system();     
+    return 0;
 }
